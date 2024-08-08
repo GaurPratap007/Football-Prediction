94af33488cac177641c5473151c8c8486518ee94
Gaurav Chauhan <103279493+GaurPratap007@users.noreply.github.com>
2024-08-08 12:24:36 +0530

    Add files via upload

---
+++ FOTMOB_API.py
@@ -0,0 +1,69 @@
+# Importing packages
+import json
+import requests
+import numpy as np
+import pandas as pd
+
+
+# Creeating a function extracts shots data from a fotmob url for a specific match and returns a pandas dataframe of the shots information
+def getFOTMOBShots(url, returnJSON=False):
+    """
+    A function that takes in a FOTMOB match URL, extracts the match ID and uses the FOTMOB API
+    to fetch shot data for that match. The function then converts the shot data into a pandas DataFrame
+    and adds the names of the two teams playing the match.
+    
+    Parameters:
+    ===========
+        url (str): A FOTMOB match URL
+    
+    Returns:
+    ========
+        pandas.DataFrame: A DataFrame containing shot data for the match
+    """
+        
+    # Extracting match ID from match URL
+    matchLink = url
+    matchId = matchLink.split("/")[4]
+
+    # Creating API link to fetch shot data for the match
+    apiLink = "https://www.fotmob.com/api/matchDetails?matchId=" + matchId
+
+    # Making request to API and loading JSON response
+    response = requests.get(apiLink)
+    data = json.loads(response.content)
+    print(json.dumps(data, indent=4))
+    # Creating a pandas DataFrame from shot data    
+    shotsData = pd.DataFrame(data["content"]["shotmap"]["shots"])
+
+    # Extract the home team and away team data from the main data dictionary
+    homeTeam = data['general']['homeTeam']
+    awayTeam = data['general']['awayTeam']
+
+    # Create a DataFrame to store the team names and join it with the shots data
+    teamNames = pd.DataFrame([homeTeam] + [awayTeam])
+    shotsData = shotsData.join(teamNames.set_index("id"), on="teamId")
+    
+    # Rename the `name` column to teamName
+    shotsData.rename(columns={'name':'teamName'}, inplace=True)
+
+    # Create new columns for the home team and away team names
+    shotsData["homeTeam"] = homeTeam["name"]
+    shotsData["awayTeam"] = awayTeam["name"]
+
+    # Create a new column to indicate whether the shot was taken by the home or away team
+    shotsData['h_a'] = np.where(shotsData["teamId"] == homeTeam["id"], 'h', 'a')
+
+    # Update the 'eventType' column to 'Blocked' for blocked shots
+    shotsData.loc[shotsData['isBlocked'], 'eventType'] = 'Blocked'
+
+    # Calculate the distance from goal
+    shotsData['distanceFromGoal'] = np.sqrt((105 - shotsData['x']) ** 2 + (34 - shotsData['y']) ** 2)
+    
+    # Extract the 'x' and 'y' values from the 'onGoalShot' column
+    shotsData['onGoalX'] = shotsData['onGoalShot'].str['x']/2
+    shotsData['onGoalY'] = shotsData['onGoalShot'].str['y']/(2/3)
+    
+    if returnJSON:
+        return shotsData, data
+    else:        
+        return shotsData
